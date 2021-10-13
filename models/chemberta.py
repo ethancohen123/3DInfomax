dc03cea511f82c9fffdb6e26a8268f5717d3a027
ethancohen123 <55588944+ethancohen123@users.noreply.github.com>
2021-10-13 17:24:43 +0200

    add chemberta encoding

--- models/chemberta.py
+++ models/chemberta.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
-import torch.optim as optim
 from torchvision import models
 
 from transformers import RobertaForMaskedLM
